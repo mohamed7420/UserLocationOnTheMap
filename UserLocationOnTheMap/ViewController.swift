7167abc666d7df19b4f57671fa74863106bef933
mohamedosama2074 <iosengks74@gmail.com>
2019-09-02 20:04:49 +0200

    Initial Commit

---
+++ UserLocationOnTheMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UserLocationOnTheMap
+//
+//  Created by Mohamed on 9/2/19.
+//  Copyright © 2019 Mohamed74. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
